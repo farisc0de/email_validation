77d09413241c05ac10eca6ad5ebe16d4348baab0
Faris AL-Otaibi <76238196+fariscode511@users.noreply.github.com>
2021-01-30 03:05:16 +0300

    Update validate.php

--- validate.php
+++ validate.php
@@ -4,7 +4,6 @@
         $email = filter_var($email, FILTER_SANITIZE_EMAIL);
         $domain = strtolower(substr($email, strpos($email, '@') + 1));
         $providers = ['gmail.com', 'hotmail.com', 'outlook.com', 'msn.com', 'outlook.sa', 'aol.com', 'protonmail.com'];
-        $inarray = in_array($domain, $providers);
-        return (filter_var($email, FILTER_VALIDATE_EMAIL) && checkdnsrr($domain) != false && $inarray);
+        return (filter_var($email, FILTER_VALIDATE_EMAIL) && in_array($domain, $providers) && checkdnsrr($domain) != false);
     }
 ?>
